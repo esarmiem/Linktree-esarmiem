df449bfca3a8eee9847658481e8e27f090d9bacc
elder sarmiento <eldersarmiento1@gmail.com>
2023-07-22 11:59:25 -0500

    add components - header/main/footer 🧩

---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<header class="bg-orange-200 px-4 py-3 text-black">
+  <p class="text-center text-sm font-medium">
+    <a href="https://github.com/esarmiem" target="_blank">
+      📲 Descubre mi perfil tecnológico y conoce los proyectos en los que estoy
+      trabajando 🔗
+    </a>
+  </p>
+</header>
