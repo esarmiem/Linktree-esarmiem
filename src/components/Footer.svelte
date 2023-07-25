752fc6f5f5533fb4ad6918b6908d03234461b75e
elder sarmiento <eldersarmiento1@gmail.com>
2023-07-24 21:54:09 -0500

    add animation - links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="flex-block max-w-xl p-4 md:pd-8 mx-auto text-center">
-  hecho con amor ❤️ esarmiem © 2023
+  <p>hecho con amor ❤️ esarmiem © 2023</p>
 </footer>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -40,7 +40,7 @@
       </div>
       <figure>
         <img
-          class="rounded-full w-48"
+          class="rounded-full w-48 "
           src="./profilecopia.jpg"
           alt="Elder Sarmiento"
         />
@@ -62,16 +62,16 @@
         </div>
       </section>
 
-      <section class="bg-orange-200 px-5 py-5 w-full flex justify-between">
+      <div class="bg-orange-200 px-5 py-5 w-full flex justify-between">
         <a
-          class="text-sm font-bold text-gray-700 text-center hover:text-yellow-800 hover:underline cursor-pointer"
+          class="text-sm font-bold text-gray-700 text-center transition duration-300 ease-in-out hover:scale-105 hover:underline cursor-pointer"
           href="https://sassy-detective-60f.notion.site/Dart-e1d4a04c176646ed9c40c1224df8620b"
           target="_blank"
         >
           Dart desde 0 • aprende con mis notas en Notion
         </a>
         <span>📖</span>
-      </section>
+      </div>
     </section>
   </section>
 </main>
