8ca17949c8fd7e65b2435c7bc3e0d63add3cd426
elder sarmiento <eldersarmiento1@gmail.com>
2023-08-14 00:23:27 -0500

    add PortfolioLink component

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,8 +1,10 @@
 <script>
+	import PortfolioLink from './linksUi/PortfolioLink.svelte';
   import DartLink from "./linksUi/DartLink.svelte";
   import GoogleLink from "./linksUi/GoogleLink.svelte";
 </script>
 
+<PortfolioLink/>
 <DartLink/>
 <GoogleLink/>
 
