3adc97616fecbc5051285d5afdbc7c14d891c9b4
elder sarmiento <eldersarmiento1@gmail.com>
2023-08-21 16:53:25 -0500

    add github component link

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -2,9 +2,11 @@
 	import PortfolioLink from './linksUi/PortfolioLink.svelte';
   import DartLink from "./linksUi/DartLink.svelte";
   import GoogleLink from "./linksUi/GoogleLink.svelte";
+  import GithubLink from './linksUi/GithubLink.svelte';
 </script>
 
 <PortfolioLink/>
 <DartLink/>
 <GoogleLink/>
+<GithubLink/>
 
