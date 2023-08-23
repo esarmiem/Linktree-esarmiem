ca0da159449f636781e4681452b4a92fcc3456b3
elder sarmiento <eldersarmiento1@gmail.com>
2023-08-23 00:01:20 -0500

    ADD getonboard link component

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -3,10 +3,12 @@
   import DartLink from "./linksUi/DartLink.svelte";
   import GoogleLink from "./linksUi/GoogleLink.svelte";
   import GithubLink from './linksUi/GithubLink.svelte';
+  import GetOnBoardLink from './linksUi/GetOnBoardLink.svelte';
 </script>
 
 <PortfolioLink/>
 <DartLink/>
 <GoogleLink/>
 <GithubLink/>
+<GetOnBoardLink/>
 
---
+++ src/components/linksUi/GetOnBoardLink.svelte
@@ -0,0 +1,12 @@
+<div class="bg-slate-100 px-5 py-5 w-full flex justify-between">
+    <a
+      class="text-sm font-bold text-gray-700 transition duration-300 ease-in-out hover:scale-105 hover:underline cursor-pointer"
+      href="https://www.getonbrd.com/p/elder-rafael-sarmiento-martinez"
+      target="_blank"
+    >
+    Get on Board
+    </a>
+    <span>
+       <img class="w-6 h-7" src="getonboard.webp" alt="getonboard">
+    </span>
+  </div>
